0bef53f7ef2bd191098a00ecf40b2e2135618bf2
lindemuthariga483 <lindemuthariga483@gmail.com>
2025-03-07 04:04:06 +0000

    Add 2.java

---
+++ src/2.java
@@ -0,0 +1,12 @@
+// A simple Java program to demonstrate how to solve a math problem
+
+public class MathProblem {
+    public static void main(String[] args) {
+        // Declare variables for the problem
+        int x = 5;
+        int y = 3;
+
+        // Solve the problem and print the result
+        System.out.println("The sum of " + x + " and " + y + " is " + (x + y));
+    }
+}
