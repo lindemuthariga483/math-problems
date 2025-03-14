785fdfab41913cb7f6ddb63f7abf11c4ce231004
lindemuthariga483 <lindemuthariga483@gmail.com>
2025-03-14 07:49:25 +0000

    Add 8.java

---
+++ src/8.java
@@ -0,0 +1,8 @@
+import java.util.Random; 
+
+public class MathProblems {
+    public static void main(String[] args) {
+        Random r = new Random();
+        System.out.println("Your random number is " + r.nextInt());
+    }
+}
