f1f04f2f1ca92a1d9f6d716e6fbb6afe88f8e83c
lindemuthariga483 <lindemuthariga483@gmail.com>
2025-04-07 04:26:07 +0000

    Add 24.java

---
+++ src/24.java
@@ -0,0 +1,14 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example of an arithmetic operation with Java
+        int result = 5 + 3;
+        System.out.println("The sum is: " + result);
+        
+        // Another example involving loops and conditionals in Java
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
