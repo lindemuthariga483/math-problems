ac98a7f5ef40908dc974f6d451385e1ac688bec7
lindemuthariga483 <lindemuthariga483@gmail.com>
2025-04-25 09:17:33 +0000

    Add 30.java

---
+++ src/30.java
@@ -0,0 +1,11 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example code for solving mathematical problems
+        int result = solveMathProblem(5, 3, 2);
+        System.out.println("The result is: " + result);
+    }
+
+    private static int solveMathProblem(int a, int b, int c) {
+        return (a * b) / c;
+    }
+}
