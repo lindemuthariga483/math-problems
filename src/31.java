1297565f162b25c3c26ae5495019d3e6810795ae
lindemuthariga483 <lindemuthariga483@gmail.com>
2025-04-27 02:20:41 +0000

    Add 31.java

---
+++ src/31.java
@@ -0,0 +1,22 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example Java program with a main method and some sample code
+
+        System.out.println("Hello, World!");
+        
+        double a = 2.0;
+        double b = 3.5;
+
+        int c = a + b;
+        double d = Math.sqrt(4);
+        String e = "Java is cool";
+
+        boolean f = true;
+        float g = -1.7f;
+
+        System.out.println("The value of a + b is: " + a + " + " + b);
+        System.out.println("c squared is: " + c);
+        System.out.println(e.equals("Java is cool"));
+        System.out.println(g == 1.7f);
+    }
+}
