a66ef3dbc23c16d3bb9b8cd2ec58a53e7d154108
lindemuthariga483 <lindemuthariga483@gmail.com>
2025-05-17 07:36:17 +0000

    Add 32.java

---
+++ src/32.java
@@ -0,0 +1,13 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example of some arithmetic operations and conditional statements
+        System.out.println("10 + 5 = " + (10 + 5));  // Output: 15
+        System.out.println("10 - 5 = " + (10 - 5));  // Output: 5
+        System.out.println("10 * 5 = " + (10 * 5));  // Output: 50
+        if (a > b) {
+            System.out.println("a is greater than b");
+        } else {
+            System.out.println("b is greater than a");
+        }
+    }
+}
