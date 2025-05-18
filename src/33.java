b26b0d36dfc3a421be934067f066bcb161393718
lindemuthariga483 <lindemuthariga483@gmail.com>
2025-05-18 02:54:15 +0000

    Add 33.java

---
+++ src/33.java
@@ -0,0 +1,6 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example of solving a simple linear equation: ax + b = c
+        double x = (c - b) / a;
+    }
+}
