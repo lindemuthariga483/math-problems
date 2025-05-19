4ed90bd299ff154499778d3b06d7e6aa67f17c1e
lindemuthariga483 <lindemuthariga483@gmail.com>
2025-05-19 19:30:31 +0000

    Add 34.java

---
+++ src/34.java
@@ -0,0 +1,7 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        int num1 = 5;
+        double num2 = 3.7;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + (num1 + num2));
+    }
+}
