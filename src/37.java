c5d6d8651062be6498718e62077cf128bd0c71cf
lindemuthariga483 <lindemuthariga483@gmail.com>
2025-05-20 14:20:19 +0000

    Add 37.java

---
+++ src/37.java
@@ -0,0 +1,10 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example code snippet with variable and loops
+        int sum = 0;
+        for (int i = 1; i <= 5; i++) {
+            sum += i;
+        }
+        System.out.println("The sum of numbers from 1 to 5 is: " + sum);
+    }
+}
