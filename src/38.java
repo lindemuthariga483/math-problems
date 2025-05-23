71d938cc12ef634e1e1a84eb813114224ff5d5b5
lindemuthariga483 <lindemuthariga483@gmail.com>
2025-05-23 07:28:52 +0000

    Add 38.java

---
+++ src/38.java
@@ -0,0 +1,8 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example math problem: find the square root of 16
+        double number = 16;
+        double squareRoot = Math.sqrt(number);
+        System.out.println("The square root of " + number + " is " + squareRoot);
+    }
+}
