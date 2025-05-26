4adc80802f74641d00f0a32dac9194f3cd8750b9
lindemuthariga483 <lindemuthariga483@gmail.com>
2025-05-26 20:00:38 +0000

    Add 41.java

---
+++ src/41.java
@@ -0,0 +1,11 @@
+public class MathProblemsSolution {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        
+        System.out.println("The sum of " + a + " and " + b + " is: " + (a + b));
+        System.out.println("The difference between " + a + " and " + b + " is: " + (a - b));
+        System.out.println("The product of " + a + " and " + b + " is: " + (a * b));
+        System.out.println("The quotient of " + a + " and " + b + " is: " + (a / b));
+    }
+}
