29eb1682c479c01bfc54e60c86d723823f6f759d
lindemuthariga483 <lindemuthariga483@gmail.com>
2025-05-27 03:23:47 +0000

    Add 42.java

---
+++ src/42.java
@@ -0,0 +1,11 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example 1: Add two numbers
+        int sum = 5 + 3;
+        System.out.println("The sum is: " + sum);
+
+        // Example 2: Subtract a number from another
+        int difference = 8 - 4;
+        System.out.println("The difference is: " + difference);
+    }
+}
